f1c7f8559b76e89c6ad860052871d318c06c358b
KarManukyan <KarManukyan@gmail.com>
2023-06-01 22:51:47 +0400

    Edited

--- Hardware/lights_7/light_7.v
+++ Hardware/lights_7/light_7.v
@@ -1,8 +1,7 @@
-module light_7 (clk, rst, number, led_1, led_2, led_3, led_4,led_5, led_6, led_7);
+module light_7 (clk, rst, led_1, led_2, led_3, led_4,led_5, led_6, led_7);
 
 input clk;
 input rst;
-input [3:0] number;
 output reg led_1;
 output reg led_2;
 output reg led_3;
@@ -15,10 +14,12 @@ reg [3:0] state;
 reg [24:0] count;
 
 always @(posedge clk) begin
-	if (count == 25'd2500000) begin
+	if (count == 25'd2_500_000) begin
 		count <= 0;
 		state <= state + 1;
-	end
+	end		
+	else if (state == 4'd10)
+		state <= 0;
 	else
 		count <= count + 1;
 
@@ -33,9 +34,20 @@ always @(*) begin
                 led_5 <= 1'b0;
                 led_6 <= 1'b0;
                 led_7 <= 1'b0;
-        end 
+        	state <= 0;
+		count <=0;
+	end 
 	else begin
-		case(number)
+		case(state)
+		4'b0000: begin
+			led_1 <= 1'b1;
+                        led_2 <= 1'b1;
+                        led_3 <= 1'b1;
+                        led_4 <= 1'b1;
+                        led_5 <= 1'b1;
+                        led_6 <= 1'b1;
+                        led_7 <= 1'b0;
+		end
 		4'b0001: begin
                         led_1 <= 1'b1;
 		        led_2 <= 1'b0;
@@ -117,10 +129,16 @@ always @(*) begin
                         led_6 <= 1'b0;
                         led_7 <= 1'b0;
                 end
-			
-		default:
-			state <= 4'b0000;
-		
+
+		default: begin
+			led_1 <= 1'b1;
+                        led_2 <= 1'b1;
+                        led_3 <= 1'b1;
+                        led_4 <= 1'b1;
+                        led_5 <= 1'b1;
+                        led_6 <= 1'b1;
+                        led_7 <= 1'b1;
+		end
 	endcase
 end	
 end
